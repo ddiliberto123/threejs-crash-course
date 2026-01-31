d3071e4383bd380adf8ad665969b55ab97cc69a8
ddiliberto123 <daniel.diliberto@hotmail.com>
2026-01-31 12:44:54 -0500

    done the tutorial for the most part

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # threejs-crash-course
 
 Following along to: https://youtu.be/KM64t3pA4fs?si=OHHEeXO98qqVfVa7
+
+Some notes:
+
+3 Main things:
+1. The scene, where all the objects live
+2. The camera, the actual perspective that the user sees. Main 2 are perspective that lets you move around and orthographic which is more stationary. The orthographic also looks a little less realistic imo since all lines in the scene are parallel to one another unlike perspective.
+3. Renderer process, responsible for rendering all the stuff in the scene
+
+A mesh the rendereable object and is made of the following: 
+1. A geometry which is broken down into triangles, the more triangles the higher the detail but the higher the GPU intensity. 
+2. A material which includes how the geometry should look. A material can have multiple things including. Changing the parameter affects the property uniformly accross the entire mesh. This however can be changed by inserting a map into the material. This map can be loaded via a texture loader. These materials can have a lot of perameters, such as:
+  1. Roughness, how rough a material looks
+  2. Metalness, how metalic/shiny a material looks
+  3. Normal, how light should bounce off the material.
+  4. Ambient Occlusion, AO, how ambient things occlude eachother.
+  5. Height, (displacement in threejs) changes the height of the mesh and therefore the actual geometry of the shape resulting more triangles to be required for more LOD.
+
+There are 2 main types of materials:
+  1. Non Environmental, these do not get affected by light and therefore do not having shading. Ex:
+    1. Mesh Basic Material
+    2. Mesh Depth Material
+  2. Environmental, environmental use physically based rendering, PBR, allowing light to change the shading of the mesh. For this reason, they have more settings that describe the way light should bounce off them. Ex in increasing order of realism:
+    1. Lambert
+    2. Phong
+    3. Normal
+    4. Physical
+
+In terms of the maps that u can provide for PBR texture set, the main ones are:
+  1. Albedo, the base texture: what it looks like, the color represented in rgb
+  2. AO, the ambient occlusion map represented in black and white
+  3. Height, the height map represented in black and white brighter often means higher
+  4. Metallic, the metalness map represented in black and white
+  5. Normal, the normal map represented in rgb
+  6. Roughness, the roughness map represented in black and white, black generally means smoother, white means rougher.
--- mesh-material/src/main.ts
+++ mesh-material/src/main.ts
@@ -1,22 +1,109 @@
-import * as three from "three";
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { Pane } from "tweakpane";
 
-const scene = new three.Scene();
+const scene = new THREE.Scene();
 const pane = new Pane();
+const textureLoader = new THREE.TextureLoader()
 
-const geometry = new three.BoxGeometry(1, 1, 1);
-const torusKnot = new three.TorusKnotGeometry(0.5, 0.2, 100, 16);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const uv2Geometry = new THREE.BufferAttribute(geometry.attributes.uv.array, 2)
+geometry.setAttribute('uv2', uv2Geometry)
+
+const torusKnot = new THREE.TorusKnotGeometry(0.5, 0.2, 100, 16);
+const uv2TorusKnot = new THREE.BufferAttribute(torusKnot.attributes.uv.array, 2)
+geometry.setAttribute('uv2', uv2TorusKnot)
+
+const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const uv2PlaneGeometry = new THREE.BufferAttribute(planeGeometry.attributes.uv.array, 2)
+geometry.setAttribute('uv2', uv2PlaneGeometry)
+
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const uv2SphereGeometry = new THREE.BufferAttribute(sphereGeometry.attributes.uv.array, 2)
+geometry.setAttribute('uv2', uv2SphereGeometry)
+
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+const uv2CylinderGeometry = new THREE.BufferAttribute(cylinderGeometry.attributes.uv.array, 2)
+geometry.setAttribute('uv2', uv2CylinderGeometry)
+
 // const material = new three.MeshBasicMaterial();
-const material = new three.MeshPhongMaterial();
+// const material = new three.MeshPhongMaterial();
+// const material = new three.MeshStandardMaterial();
 
-// material.color = new three.Color("white");
+const grassAlbedo = textureLoader.load('./static/textures/grassy-meadow1-bl/grassy-meadow1_albedo.png')
+// grassAlbedo.repeat.set(2,2);
+// grassAlbedo.wrapS = THREE.MirroredRepeatWrapping;
+// grassAlbedo.wrapT = THREE.MirroredRepeatWrapping;
+const grassAo = textureLoader.load('./static/textures/grassy-meadow1-bl/grassy-meadow1_ao.png')
+const grassHeight = textureLoader.load('./static/textures/grassy-meadow1-bl/grassy-meadow1_height.png')
+const grassMetallic = textureLoader.load('./static/textures/grassy-meadow1-bl/grassy-meadow1_metallic.png')
+const grassNormal = textureLoader.load('./static/textures/grassy-meadow1-bl/grassy-meadow1_normal-ogl.png')
+const grassRoughness = textureLoader.load('./static/textures/grassy-meadow1-bl/grassy-meadow1_roughness.png')
 
-pane.addBinding(material, "shininess", {
+
+// grassTexture.offset.x = 0.5
+// pane.addBinding(grassAlbedo, "offset", {
+//   x: {
+//     min: -1,
+//     max: 1,
+//     step: 0.01,
+//   },
+//   y: {
+//     min: -1,
+//     max: 1,
+//     step: 0.01,
+//   },
+// });
+
+const material = new THREE.MeshPhysicalMaterial();
+const material2=  new THREE.Mesh();
+material.map = grassAlbedo;
+material.roughnessMap = grassRoughness;
+material.metalnessMap = grassMetallic;
+material.normalMap = grassNormal;
+material.aoMap = grassAo;
+
+pane.addBinding(material, 'aoMapIntensity', {
   min: 0,
-  max: 100,
-  step: 1,
-});
+  max: 1,
+  step: 0.01
+})
+/**
+ * Changes the actual geometry of the mesh resulting in more triangles
+ */
+// material.displacementMap = grassHeight;
+// material.displacementScale = .1;
+
+material.aoMap = grassAo;
+
+const group = new THREE.Group();
+
+
+material.color = new THREE.Color("green");
+
+// pane.addBinding(material, "metalness", {
+//   min: 0,
+//   max: 1,
+//   step: 0.01,
+// });
+
+// pane.addBinding(material, "roughness", {
+//   min: 0,
+//   max: 1,
+//   step: 0.01,
+// });
+
+// pane.addBinding(material, "reflectivity", {
+//   min: 0,
+//   max: 1,
+//   step: 0.01,
+// });
+
+// pane.addBinding(material, "clearcoat", {
+//   min: 0,
+//   max: 1,
+//   step: 0.01,
+// });
 
 // material.transparent = true;
 // material.opacity = .5;
@@ -27,32 +114,51 @@ pane.addBinding(material, "shininess", {
 // scene.fog = fog;
 // scene.background = new three.Color('white')
 
-const mesh = new three.Mesh(geometry, material);
-const mesh2 = new three.Mesh(torusKnot, material);
-mesh2.position.z = 2;
+const cube = new THREE.Mesh(geometry, material);
+cube.position.x = 2;
 
-const camera = new three.PerspectiveCamera(
+const knot = new THREE.Mesh(torusKnot, material);
+
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.x = -2;
+// plane.rotation.x = Math.PI * .5
+// plane.material.side = THREE.DoubleSide;
+// plane.scale.set(100, 100, 1)
+
+const sphere = new THREE.Mesh();
+sphere.geometry = sphereGeometry;
+sphere.material = material;
+sphere.position.z = -2;
+
+const cylinder = new THREE.Mesh();
+cylinder.geometry = cylinderGeometry;
+cylinder.material = material;
+cylinder.position.z = 2;
+
+const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  30,
+  2000,
 );
 
-scene.add(mesh);
-scene.add(mesh2);
+group.add(sphere, cylinder, cube, knot, plane);
+// scene.add(plane)
+scene.add(group);
 scene.add(camera);
-const light = new three.AmbientLight('white', 0.1);
-scene.add(light)
+const light = new THREE.AmbientLight("white", .5);
+scene.add(light);
 
-const pointLight = new three.PointLight("white", 40);
+const pointLight = new THREE.PointLight("white", 40);
 pointLight.position.set(5, 5, 2);
 scene.add(pointLight);
 
 const canvas = document.querySelector("canvas.threejs") as HTMLCanvasElement;
 
 camera.position.z = 5;
+camera.position.y = 5
 
-const renderer = new three.WebGLRenderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
 });
@@ -69,7 +175,12 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
+console.log(scene.children);
+
 const renderloop = () => {
+  group.children.forEach((child) => {
+    // child.rotation.y += 0.01;
+  });
   renderer.render(scene, camera);
 
   controls.update();
